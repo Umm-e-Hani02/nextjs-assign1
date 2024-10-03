3268cfe74ed1232da2e7bb4d8da14c561ae40175
Umm-e-Hani02 <165858620+Umm-e-Hani02@users.noreply.github.com>
2024-10-03 21:17:58 +0500

    changed background colour

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`bg-teal-400 ${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Navbar/>
         {children}
