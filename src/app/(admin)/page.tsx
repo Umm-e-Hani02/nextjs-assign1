ba5b016d9cd72851c062efe876834cd5395f43ba
Umm-e-Hani02 <165858620+Umm-e-Hani02@users.noreply.github.com>
2024-10-03 21:20:35 +0500

    Update page.tsx

--- src/app/(admin)/page.tsx
+++ src/app/(admin)/page.tsx
@@ -6,7 +6,7 @@ export default function Home(){
             <p className="text-xl flex justify-center items-center p-7">Explore Some Countries with their capital and population.</p>
             <div className="flex justify-center items-center">
             <Link href="/country">
-            <button className="bg-slate-900 text-white py-2 px-4 rounded mt-60 hover:text-gray-200 active:bg-slate-800 transition-all duration-300 ease-in-out">Explore countries</button>
+            <button className="bg-blue-950 text-white py-2 px-4 rounded mt-60 hover:text-gray-200 active:bg-slate-800 transition-all duration-300 ease-in-out">Explore countries</button>
             </Link>   
             </div>
         </div>
