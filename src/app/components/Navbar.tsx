e569a0a3a9535ef2ea139e931c441a4421e22b6e
Umm-e-Hani02 <165858620+Umm-e-Hani02@users.noreply.github.com>
2024-10-03 21:19:34 +0500

    Update Navbar.tsx

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,8 +1,8 @@
 import Link from "next/link"
 export default function Navbar(){
     return(
-        <nav className="bg-slate-950 p-6">
-            <div className="flex justify-center items-center gap-9" >
+        <nav className="bg-blue-950 p-6">
+            <div className="flex justify-center items-center gap-9 text-nowrap" >
                 <Link href="/">Home</Link>
                 <Link href="/aboutus">About us</Link>
                 <Link href="/contactus">Contact us</Link>
